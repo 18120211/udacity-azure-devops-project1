7b095be568f6fed00b5e4127186c509f1d96bbed
Minh Vo <minhthevo123@gmail.com>
2022-11-21 15:21:29 +0700

    some update

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,24 +15,19 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 
-  subnet {
-    name           = "subnet-private-${var.project_name}"
-    address_prefix = "10.0.1.0/24"
-    security_group = azurerm_network_security_group.sg.id
-  }
 }
 
 resource "azurerm_subnet" "subnet_private" {
   name                 = "subnet-private-${var.project_name}"
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.rg.name
-  address_prefixes     = ["10.0.2.0/24"]
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                 = "nic-${var.project_name}"
-  location             = azurerm_resource_group.rg.location
-  resource_group_name  = azurerm_resource_group.rg.name
+  name                = "nic-${var.project_name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet_private.id
@@ -64,8 +59,58 @@ resource "azurerm_lb_backend_address_pool" "backendpool" {
   name            = "backendpool-${var.project_name}"
 }
 
-resource "azurerm_availability_set" "vmas" {
-  name                = "vmas-${var.project_name}"
+
+resource "azurerm_virtual_machine_scale_set" "vmss" {
+  name                = "vmss-${var.project_name}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  upgrade_policy_mode = "Manual"
+
+  sku {
+    name     = var.vm_size
+    tier     = "Standard"
+    capacity = 2
+  }
+
+  storage_profile_image_reference {
+    id = data.azurerm_image.image.id
+  }
+
+  storage_profile_os_disk {
+    name              = ""
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_profile_data_disk {
+    lun           = 0
+    caching       = "ReadWrite"
+    create_option = "Empty"
+    disk_size_gb  = 10
+  }
+
+  os_profile {
+    computer_name_prefix = "web-server"
+    admin_username       = var.admin_user
+    admin_password       = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  network_profile {
+    name    = "terraformnetworkprofile"
+    primary = true
+
+    ip_configuration {
+      name                                   = "IPConfiguration"
+      subnet_id                              = azurerm_subnet.subnet_private.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.backendpool.id]
+      primary                                = true
+    }
+  }
+
+  tags = var.tags
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,28 @@ variable "project_name" {
   type = string
   default = "udacity-project-1"
 }
+
+variable "packer_resource_group_name" {
+  type = string
+  default = "rg-udacity-images"
+}
+
+variable "packer_image_name" {
+  type = string
+  default = "im-azure-project1"
+}
+
+variable "admin_username" {
+  type = string
+  default = "admin"
+}
+
+variable "admin_password" {
+  type = string
+  default = "admin"
+}
+
+variable "vm_size" {
+  type = string
+  default = "Standard_B1s"
+}
